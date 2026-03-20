5d20d3b046a3ed8faa26806b31ba162f52af790f
Bruna Suzuki <brunasuzuki04@gmail.com>
2026-03-20 18:16:35 -0300

    initial commit

---
+++ src/main/java/com/livoniaBank/LivoniaBankApplication.java
@@ -0,0 +1,13 @@
+package com.livoniaBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivoniaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivoniaBankApplication.class, args);
+	}
+
+}
